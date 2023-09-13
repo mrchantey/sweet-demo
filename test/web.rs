a96f3680d04e0785159271a9c32429ba1192a4e8
Peter Hayman <25616826+mrchantey@users.noreply.github.com>
2023-09-13 12:04:15 +1000

    add in-browser e2e tests

--- test/web.rs
+++ test/web.rs
@@ -2,12 +2,18 @@ use sweet::*;
 use web_sys::window;
 
 sweet! {
-	it "does web stuff" {
-		window().unwrap()
-			.document().unwrap()
-			.body().unwrap()
-			.set_inner_html("<h1>This is a heading</h1>");
-		expect(window()).get("h1")?
-			.to_contain_text("This is a heading")?;		
-	}
+  test "a component test" {
+    window().unwrap()
+      .document().unwrap()
+      .body().unwrap()
+      .set_inner_html("<h1>This is a heading</h1>");
+    expect(window()).get("h1")?
+      .to_contain_text("This is a heading")?;
+  }
+
+  test e2e "an e2e test"{
+      let page = visit("http://example.com").await?;
+    expect(page)
+      .poll(|p|p.to_contain_text("This domain is for use in illustrative examples")).await?;
+  }
 }
