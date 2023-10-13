0c33f6bd711767cac79828804a405a5e3330948c
Peter Hayman <25616826+mrchantey@users.noreply.github.com>
2023-10-13 17:12:02 +1100

    fix: broken test

--- test/web_e2e.rs
+++ test/web_e2e.rs
@@ -4,6 +4,6 @@ sweet! {
   test e2e "an e2e test"{
     let page = visit("http://example.com").await?;
     expect(page)
-      .poll(|p|p.to_contain_text("for use in illustrative exjkljamples")).await?;
+      .poll(|p|p.to_contain_text("for use in illustrative examples")).await?;
   }
 }
